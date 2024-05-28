60ad7f7fe4d28c650d013303a87dfb5e5f011491
Renan Ramos <renan_dasilvaramos@hotmail.com>
2024-05-28 14:39:44 -0300

    feat : CRUD Setor de atuação

---
+++ src/main/java/com/renovatech/RenovaTechApplication.java
@@ -0,0 +1,13 @@
+package com.renovatech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RenovaTechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RenovaTechApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/renovatech/repository/SetorAtuacaoRepository.java
@@ -0,0 +1,14 @@
+package com.renovatech.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+
+import com.renovatech.model.SetorAtuacao;
+
+public interface SetorAtuacaoRepository extends JpaRepository<SetorAtuacao,Long>{
+	
+	public List<SetorAtuacao> findAllByNomeContainingIgnoreCase(@Param("nome") String nome);
+
+}
