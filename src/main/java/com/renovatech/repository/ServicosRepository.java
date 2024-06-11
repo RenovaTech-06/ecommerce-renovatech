dfe4284a130b1b868af95961f0abac22ff495ec4
Your Name <you@example.com>
2024-06-11 17:19:53 -0300

    feat:atualizando SwaggerConfig

--- src/main/java/com/renovatech/repository/ServicosRepository.java
+++ src/main/java/com/renovatech/repository/ServicosRepository.java
@@ -8,9 +8,5 @@ import com.renovatech.model.Servicos;
 
 public interface ServicosRepository extends JpaRepository<Servicos, Long>{
 	
-	
-<<<<<<< HEAD
 
-=======
->>>>>>> b09fbf0a93b5baeac500aa5a2438508e49b26b7e
 }
